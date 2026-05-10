cb78796e4e69eca4936c7a3c8ccfb0e44905c55a
hoanghunglb <hoanghunglb1@gmail.com>
2026-05-10 14:18:05 +0700

    Fix Tusachxinhxinh mobile install package

--- tusachxinhxinh/src/chap.js
+++ tusachxinhxinh/src/chap.js
@@ -1,41 +1,8 @@
 load('config.js');
-load('crypto-js.min.js');
 
 let IMAGE_DOT = "qX3xRL";
 let IMAGE_COLON = "guhD2Z";
 let IMAGE_SLASH = "9f7sWJ";
-let PASS_PHRASE = IMAGE_DOT + IMAGE_COLON + IMAGE_SLASH;
-
-function scriptString(value) {
-    try {
-        return JSON.parse("\"" + value + "\"");
-    } catch (error) {
-        return value.replace(/\\\//g, "/").replace(/\\"/g, "\"").replace(/\\\\/g, "\\");
-    }
-}
-
-function encryptedChapterJson(html) {
-    let match = /var\s+htmlContent\s*=\s*"((?:\\.|[^"\\])*)"/i.exec(html || "");
-    return match ? scriptString(match[1]) : "";
-}
-
-function decryptChapterHtml(encryptedJson) {
-    if (!encryptedJson) return "";
-    try {
-        let obj = JSON.parse(encryptedJson);
-        let salt = CryptoJS.enc.Hex.parse(obj.salt);
-        let iv = CryptoJS.enc.Hex.parse(obj.iv);
-        let key = CryptoJS.PBKDF2(PASS_PHRASE, salt, {
-            hasher: CryptoJS.algo.SHA512,
-            keySize: 64 / 8,
-            iterations: 999
-        });
-        let decrypted = CryptoJS.AES.decrypt(obj.ciphertext, key, {iv: iv});
-        return decrypted.toString(CryptoJS.enc.Utf8);
-    } catch (error) {
-        return "";
-    }
-}
 
 function decodeChapterImage(url) {
     if (!url) return "";
@@ -117,11 +84,6 @@ function execute(url) {
 
     let html = getText(url);
     if (!html) return Response.error("Khong tai duoc chapter.");
-
-    let decrypted = decryptChapterHtml(encryptedChapterJson(html));
-    collectEncodedImagesFromText(decrypted, data, seen, url);
-    if (data.length === 0) collectImagesFromHtml(decrypted, data, seen, url);
-    if (data.length > 0) return Response.success(data);
 
     collectImagesFromText(html, data, seen, url);
     if (data.length === 0) {
